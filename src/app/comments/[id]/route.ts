8c25ffd96e00ee16bf0db772b14f5de495e0569e
gufran0 <md.gufran@techchefz.com>
2024-11-29 01:30:41 +0530

    parallel routes

---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,34 @@
+import { comments } from "../data";
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  if (comment) {
+    return Response.json(comment);
+  } else {
+    return Response.json({ message: "Comment not found" }, { status: 404 });
+  }
+}
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+  const comment = await request.json();
+  const updatedComment = { ...comment, id: parseInt(params.id) };
+  return Response.json(updatedComment);
+}   
+
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const commentIndex = comments.findIndex   (
+    (comment) => comment.id === parseInt(params.id)
+  );
+  const deletedComment = comments.splice(commentIndex, 1);
+  return Response.json(deletedComment);
+}
+
